0d875838e2e174e23fb162d4455ed84850563b64
EJ <ej.roux13@gmail.com>
2021-05-10 00:11:50 -0700

    updated server to include env variables for deployment

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
+const chalk = require("chalk");
 
-const PORT = 3000;
+// Update PORT for Heroku deployment
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -15,7 +17,8 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+// Update connection for MongoDB Atlas and Heroku
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
@@ -24,5 +27,5 @@ mongoose.connect("mongodb://localhost/budget", {
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+  console.log(chalk.bold.green(`App running on port ${PORT}!`));
 });
