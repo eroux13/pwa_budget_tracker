8901e709d9a4939592585ad8b7aaceab9c5cbbaa
EJ <ej.roux13@gmail.com>
2021-05-20 17:32:59 -0700

    added fetch event listener

--- public/service-worker.js
+++ public/service-worker.js
@@ -50,3 +50,35 @@ self.addEventListener("activate", function(event) {
     )
     self.ClientRectList.claim();
 });
+
+// Fetch
+self.addEventListener("fetch", function(event) {
+    if(event.request.url.includes("/api/")){
+        event.respondWith(
+            caches.open(DATA_CACHE_NAME)
+                .then(cache => {
+                    return fetch(event.request)
+                        .then(response => {
+                            // If the response was good, clone it and store it in the cache.
+                            if (response.status === 200) {
+                                cache.put(event.request.url, response.clone())
+                            }
+                            return response;
+                        })
+                })
+                .catch(err => {
+                    // Network request failed, try to get it from the cache.
+                    return cache.match(event.request);
+                })
+        ).catch(err => console.log(err))
+        return;
+    }
+    event.respondWith(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                return cache.match(event.request).then(response => {
+                    return response || fetch(event.request);
+                });
+            })
+    )
+});
