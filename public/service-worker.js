38085cdebd5b7acf4052b5b731a80a954b6c9488
EJ <ej.roux13@gmail.com>
2021-05-21 09:19:23 -0700

    fixed bug in service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -48,29 +48,29 @@ self.addEventListener("activate", function(event) {
                 )
             })
     )
-    self.ClientRectList.claim();
+    self.clients.claim();
 });
 
 // Fetch
 self.addEventListener("fetch", function(event) {
-    if(event.request.url.includes("/api/")){
+    if (event.request.url.includes("/api/")) {
         event.respondWith(
-            caches.open(DATA_CACHE_NAME)
-                .then(cache => {
-                    return fetch(event.request)
-                        .then(response => {
-                            // If the response was good, clone it and store it in the cache.
-                            if (response.status === 200) {
-                                cache.put(event.request.url, response.clone())
-                            }
-                            return response;
-                        })
+          caches.open(DATA_CACHE_NAME)
+            .then(cache => {
+                return fetch(event.request)
+                    .then(response => {
+                        // If the response was good, clone it and store it in the cache.
+                        if (response.status === 200) {
+                            cache.put(event.request.url, response.clone());
+                        }
+                    return response;
                 })
                 .catch(err => {
                     // Network request failed, try to get it from the cache.
                     return cache.match(event.request);
-                })
-        ).catch(err => console.log(err))
+                });
+            }).catch(err => console.log(err))
+        );
         return;
     }
     event.respondWith(
